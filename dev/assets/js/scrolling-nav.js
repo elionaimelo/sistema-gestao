18f4f1fcfa9ee8cd07646e436d2c41449e6e1d02
Elionai Melo <elionai.melo@gmail.com>
2015-11-16 16:42:24 -0300

    Conclusão da página inicial com responsividade

--- dev/assets/js/scrolling-nav.js
+++ dev/assets/js/scrolling-nav.js
@@ -1,13 +1,13 @@
-// jQuery to collapse the navbar on scroll
+//jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 150) {
+    if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
+//jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
@@ -16,9 +16,4 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
-});
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
 });
